a250e4ea439ba852457bfccceaa7690973e4a879
Thanh Quy <quythanhhuynh2003@gmail.com>
2023-01-19 14:54:18 +0700

    not working husky,not fix

--- src/index.js
+++ src/index.js
@@ -21,13 +21,14 @@ app.use(express.json()); //js
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('hbs',engine({
+app.engine(
+    'hbs',
+    engine({
         extname: '.hbs',
     }),
 );
-    app.set('view engine', 'hbs');
-
-    app.set('views', path.join(__dirname, './resources/views'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './resources/views'));
 
 route(app);
 
